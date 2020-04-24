b616cd6f1c298761bc1510d301523720d6e028e5
Juan Jacobo Giraldo Rodriguez <juanjacobogiraldorodriguez@Juans-MacBook-Air-2.local>
2020-04-24 19:25:16 -0400

    Este programa se ve pero mas lindo :)

---
+++ Laboratorio05/ChangePasswordEventArgs.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Laboratorio05
+{
+    public class ChangePasswordEventArgs
+    {
+        public string Email { get; set; }
+        public string Number { get; set; }
+        public string Username { get; set; }
+    }
+}
---
+++ Laboratorio05/RegisterEventArgs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Laboratorio05
+{
+    public class RegisterEventArgs
+    {
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string VerificationLink { get; set; }
+    }
+}
